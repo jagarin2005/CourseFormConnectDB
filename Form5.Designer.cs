006dbe410d860d28cbf405422839c3b981ae8ccf
jakarin_a <jakarin_a@softsquaregroup.com>
2021-01-16 12:49:33 +0700

    modified form5 datagridview save to database

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -29,12 +29,49 @@ namespace CourseFormConnectDB
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 74);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(660, 299);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(25, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(705, 397);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form5";
             this.Text = "Form5";
+            this.Load += new System.EventHandler(this.Form5_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Form5.cs
+++ Form5.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,59 @@ namespace CourseFormConnectDB
 {
     public partial class Form5 : Form
     {
+        private MySqlConnection conn;
+        private MySqlDataAdapter adpt;
+        private MySqlCommandBuilder cmbd;
+        private DataTable dt;
+        private BindingSource bds;
+        private string strQuery;
+
         public Form5()
         {
             InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            var connectionString = "Server=localhost;Database=formdb;Uid=root;Pwd=;";
+            conn = new MySqlConnection(connectionString);
+            strQuery = "SELECT * FROM form5";
+
+            conn.Open();
+
+            adpt = new MySqlDataAdapter(strQuery, conn);
+            cmbd = new MySqlCommandBuilder(adpt);
+
+            dt = new DataTable();
+            adpt.Fill(dt);
+            bds = new BindingSource();
+            bds.DataSource = dt;
+
+            dataGridView1.DataSource = bds;
+
+            conn.Close();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            LoadData();
+
+            // if you want to hide Identity column
+            dataGridView1.Columns[0].Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                adpt.Update(dt);
+                LoadData();
+                MessageBox.Show("บันทึกข้อมูลสำเร็จ", "บันทึกข้อมูลสำเร็จแล้ว", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
     }
 }
